9f0bbf59bf4ab0f678d2e02f12a9e326a6193811
Tatjana <taninjawi@mail.ru>
2022-08-23 22:44:07 +0700

    Добавила задачу 38

---
+++ task38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите
+// разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = { 5, 93, 3, 59, 6, 4, 5, 19, 8 };
+double max = array[0];
+    double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max=array[i];
+    }
+    else if (min > array[i])
+    {
+        min =array[i];
+    }
+}
+Console.WriteLine(max - min);
